7259053b22a2ed910b2410be3ac9f14c8a9cb0c5
郵差 <a34007024@gmail.com>
2021-04-17 22:54:35 +0800

    add res.writeHead()

--- W3schoolsTutorialFiles/NodeJS_HTTP_Module/demo_http.js
+++ W3schoolsTutorialFiles/NodeJS_HTTP_Module/demo_http.js
@@ -2,6 +2,10 @@ var http = require('http');
 
 //create a server object:
 http.createServer(function(req, res){
+	res.writeHead(200, {'Content-Type': 'text/html'});
+	// The first argument of the res.writeHead() method is the status code,
+	// 200 means that all is OK,
+	// the second argument is an object containing the response headers.
 	res.write('Hello World!'); //write a response to the client
 	res.end; //end the response
 }).listen(8080); //the server object listens on port 8080
