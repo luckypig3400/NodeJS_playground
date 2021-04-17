a7e643379f378d8bfa45f70d92bf56347f35cb23
郵差 <a34007024@gmail.com>
2021-04-17 23:04:21 +0800

    response user require url

--- W3schoolsTutorialFiles/NodeJS_HTTP_Module/demo_http_url.js
+++ W3schoolsTutorialFiles/NodeJS_HTTP_Module/demo_http_url.js
@@ -2,5 +2,7 @@ var http = require('http');
 
 http.createServer(function(req, res){
 	res.writeHead(200, {'Content-Type': 'text/html'});
+	res.write(req.url);
+	// try to open http://localhost:8080/summer in browser
 	res.end();
 }).listen(8080);
