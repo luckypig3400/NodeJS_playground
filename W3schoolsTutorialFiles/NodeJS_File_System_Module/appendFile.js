9332127e695f274874aaa8d03cce98e7e35171d2
郵差 <a34007024@gmail.com>
2021-04-20 14:25:35 +0800

    add appendFile() description

--- W3schoolsTutorialFiles/NodeJS_File_System_Module/appendFile.js
+++ W3schoolsTutorialFiles/NodeJS_File_System_Module/appendFile.js
@@ -4,3 +4,6 @@ fs.appendFile('mynewfile1.txt', 'Hello content!', function (err) {
     if (err) throw err;
     console.log('Saved!');
 });
+
+//The fs.appendFile() method appends specified content to a file.
+//If the file does not exist, the file will be created.
