30821d4f6e3f5082cdbcf6d14adc04abbf630b2d
郵差 <a34007024@gmail.com>
2021-04-20 18:32:16 +0800

    add description for unlink()

--- W3schoolsTutorialFiles/NodeJS_File_System_Module/delete_file_unlink.js
+++ W3schoolsTutorialFiles/NodeJS_File_System_Module/delete_file_unlink.js
@@ -4,3 +4,6 @@ fs.unlink('mynewfile2.txt', function (err) {
     if (err) throw err;
     console.log('File Deleted!');
 });
+// To delete a file with the File System module,
+// use the fs.unlink() method.
+// The fs.unlink() method deletes the specified file.
