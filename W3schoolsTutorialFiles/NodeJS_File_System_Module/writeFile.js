be1db8c2a54de19feeb96aa5d925a9e55bbb59c1
郵差 <a34007024@gmail.com>
2021-04-20 14:46:14 +0800

    add description for writeFile()

--- W3schoolsTutorialFiles/NodeJS_File_System_Module/writeFile.js
+++ W3schoolsTutorialFiles/NodeJS_File_System_Module/writeFile.js
@@ -6,3 +6,7 @@ fs.writeFile('mynewfile3.txt', 'Hello Content!',
         console.log('Saved!');
     }
 );
+
+//The fs.writeFile() method replaces the specified file and content if it exists.
+// If the file does not exist, a new file, 
+// containing the specified content, will be created.
