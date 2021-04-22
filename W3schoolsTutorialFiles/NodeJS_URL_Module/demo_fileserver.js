96a080f509fbb41527221811efdf4b27140c90af
郵差 <a34007024@gmail.com>
2021-04-22 22:31:18 +0800

    add description for this demo

--- W3schoolsTutorialFiles/NodeJS_URL_Module/demo_fileserver.js
+++ W3schoolsTutorialFiles/NodeJS_URL_Module/demo_fileserver.js
@@ -1,3 +1,7 @@
+// Create a Node.js file that opens the requested file and 
+// returns the content to the client. 
+// If anything goes wrong, throw a 404 error.
+
 var http = require('http');
 var url = require('url');
 var fs = require('fs');
@@ -9,10 +13,11 @@ http.createServer(function (req, res) {
     fs.readFile(filename, function (err, data) {
         if (err) {
             res.writeHead(404, { 'Content-Type': 'text/html' });
-            return res.end("404 Not Found (*>﹏<*)");
+            return res.end("404 Not Found QAQ");
         }
         res.writeHead(200, { 'Content-Type': 'text/html' });
         res.write(data);
         return res.end();
     });
+
 }).listen(8080);
