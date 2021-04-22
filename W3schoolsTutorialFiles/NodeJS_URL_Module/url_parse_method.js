d258a8584924d5f70f25267aa46f799b9a2d7cc0
郵差 <a34007024@gmail.com>
2021-04-22 22:13:40 +0800

    Create url_parse_method.js

---
+++ W3schoolsTutorialFiles/NodeJS_URL_Module/url_parse_method.js
@@ -0,0 +1,15 @@
+// Parse an address with the url.parse() method,
+// and it will return a URL object with each 
+// part of the address as properties.
+
+var url = require('url');
+var adr = 'http://localhost:8080/default.htm?year=2017&month=february';
+var q = url.parse(adr, true);
+
+console.log(q.host);
+console.log(q.pathname);
+console.log(q.search);
+
+var qdata = q.query;
+
+console.log(qdata.month);
