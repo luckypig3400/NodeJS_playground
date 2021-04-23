7bcc924ee86ba239a3664e86256e48762b8056dd
郵差 <a34007024@gmail.com>
2021-04-23 22:59:11 +0800

    add more eventHandler for fun

--- W3schoolsTutorialFiles/NodeJS_Events/demo_eventEmitter.js
+++ W3schoolsTutorialFiles/NodeJS_Events/demo_eventEmitter.js
@@ -3,7 +3,7 @@ var eventEmitter = new events.EventEmitter();
 
 //Create an event handler:
 var myEventHandler = function () {
-    console.log('T hear a scream!');
+    console.log('I hear a scream!');
 }
 
 //Assign the event handler to an event:
@@ -11,3 +11,16 @@ eventEmitter.on('scream', myEventHandler);
 
 //Fire the 'scream' event:
 eventEmitter.emit('scream');
+
+var helloEventHandler = function () {
+    console.log('Hello World~');
+}
+var worldEventHandler = function () {
+    console.log('World~ Hello from Node.js');
+}
+
+eventEmitter.on('hello', helloEventHandler);
+eventEmitter.on('world', worldEventHandler);
+
+eventEmitter.emit('hello');
+eventEmitter.emit('world');
