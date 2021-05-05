af121170c3974eb6e335d4b9eecee960a013018e
郵差 <a34007024@gmail.com>
2021-05-05 15:45:23 +0800

    add note for express first try

--- Express_first_try/app.js
+++ Express_first_try/app.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const app = express();
 const port = 3000;
-
-app.get('/', (req, res) => {
+//ref1: http://expressjs.com/en/starter/hello-world.html
+app.get('/', function (req, res) {
     res.send('Hello World');
+    // (req, res) => { } is equal to function(req, res) { }
 });
-
+//ref2: https://developer.mozilla.org/zh-TW/docs/Learn/Server-side/Express_Nodejs/development_environment
 app.listen(port, () => {
     console.log('Example app listening at http://localhost:' + port);
 });
