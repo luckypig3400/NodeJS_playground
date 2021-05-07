61038dc148bfd134fd297afaf25afc789a56c3e9
郵差 <a34007024@gmail.com>
2021-05-07 11:15:30 +0800

    parse the uploaded file

--- W3schoolsTutorialFiles/NodeJS_Upload_Files/demo_Upload_file.js
+++ W3schoolsTutorialFiles/NodeJS_Upload_Files/demo_Upload_file.js
@@ -1,10 +1,20 @@
 var http = require("http");
+var formidable = require('formidable');
 
 http.createServer(function (req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.write('<form action="fileupload" method="POST" enctype="multipart/form-data">');
-    res.write('<input type="file" name="fileToUpload"><br>');
-    res.write('<input type="submit">');
-    res.write('</form>');
-    return res.end();
+    if (req.url == '/fileupload') {
+        var form = new formidable.IncomingForm();
+        form.parse(req, function (err, fields, files) {
+            res.write('File Uploaded ~');
+            res.end();
+        });
+    }
+    else {
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.write('<form action="fileupload" method="POST" enctype="multipart/form-data">');
+        res.write('<input type="file" name="fileToUpload"><br>');
+        res.write('<input type="submit">');
+        res.write('</form>');
+        return res.end();
+    }
 }).listen(8080);
