58f94aae79ad4a67fc8ef3a3469d33ff4430c2c8
郵差 <a34007024@gmail.com>
2021-05-07 15:24:17 +0800

    Create demo_send_mail.js

---
+++ W3schoolsTutorialFiles/NodeJS_Send_Email/demo_send_mail.js
@@ -0,0 +1,24 @@
+var nodemailer = require('nodemailer');
+
+var transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: 'nodejsMailSender666@gmail.com',
+        pass: '3edc6yhn9ol.'
+    }
+});
+
+var mailOptions = {
+    from: 'nodejsMailSender666@gmail.com',
+    to: 'myfriend@yahoo.com',
+    subject: 'Sending Email using Node.JS 酷耶~',
+    text: 'That was Easy!'
+};
+
+transporter.sendMail(mailOptions, function (error, info) {
+    if (error) {
+        console.log(error);
+    } else {
+        console.log('Email sent: ' + info.response);
+    }
+});
