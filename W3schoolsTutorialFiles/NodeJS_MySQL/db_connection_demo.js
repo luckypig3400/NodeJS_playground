4ea56d983960359a07f75da648ea848bdb53a0d2
郵差 <a34007024@gmail.com>
2021-05-07 18:50:09 +0800

    query in DB and close connection

--- W3schoolsTutorialFiles/NodeJS_MySQL/db_connection_demo.js
+++ W3schoolsTutorialFiles/NodeJS_MySQL/db_connection_demo.js
@@ -9,4 +9,10 @@ var con = mysql.createConnection({
 con.connect(function (err) {
     if (err) throw err;
     console.log("Connected!");
+    var sql = "select * from mysql.user";
+    con.query(sql, function (err, result) {
+        if (err) throw err;
+        console.log("Result:" + result);
+        con.destroy();//close connection after query data sent back
+    });
 });
