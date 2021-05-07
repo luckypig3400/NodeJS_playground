acb12c2868f51f0a155e510def20bc6ebcf68708
郵差 <a34007024@gmail.com>
2021-05-07 11:49:09 +0800

    save uploaded file to specified folder

--- W3schoolsTutorialFiles/NodeJS_Upload_Files/demo_Upload_file.js
+++ W3schoolsTutorialFiles/NodeJS_Upload_Files/demo_Upload_file.js
@@ -1,12 +1,20 @@
 var http = require("http");
 var formidable = require('formidable');
+var fs = require('fs');
 
 http.createServer(function (req, res) {
     if (req.url == '/fileupload') {
         var form = new formidable.IncomingForm();
         form.parse(req, function (err, fields, files) {
-            res.write('File Uploaded ~');
-            res.end();
+            var oldpath = files.fileToUpload.path;
+            var newpath = './UserUploadedFiles/' + files.fileToUpload.name;
+            console.log('File Saved At Cache Folder:' + oldpath);
+            fs.rename(oldpath, newpath, function (err) {
+                if (err) throw err;
+                res.write('File Uploaded and Moved!');
+                res.end();
+                console.log('File Saved At:' + newpath);
+            });
         });
     }
     else {
