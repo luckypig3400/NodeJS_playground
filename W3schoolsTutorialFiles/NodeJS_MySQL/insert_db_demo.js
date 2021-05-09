31cfaf832b91ca3ff2ef27730ebbee9d77032586
郵差 <a34007024@gmail.com>
2021-05-09 21:47:45 +0800

    Create insert_db_demo.js

---
+++ W3schoolsTutorialFiles/NodeJS_MySQL/insert_db_demo.js
@@ -0,0 +1,21 @@
+//https://www.w3schools.com/nodejs/nodejs_mysql_insert.asp
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+    host: "localhost",
+    user: "nodejsUser",
+    password: "nodejs1234",
+    database: "mynodejsDB"
+});
+
+con.connect(function (err) {
+    if (err) throw err;
+    console.log("Connected~!");
+
+    var sql = "INSERT INTO customers(name, address) " +
+        "VALUES ('Company Inc.', 'Highway 37')";
+    con.query(sql, function (err, result) {
+        if (err) throw err;
+        console.log("1 record inserted");
+    });
+});
