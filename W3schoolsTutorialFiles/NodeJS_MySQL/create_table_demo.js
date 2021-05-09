7969761724f0cd9f6bc1ccb2befb721ee81ecd43
郵差 <a34007024@gmail.com>
2021-05-09 14:49:02 +0800

    create table with PRIMARY KEY

--- W3schoolsTutorialFiles/NodeJS_MySQL/create_table_demo.js
+++ W3schoolsTutorialFiles/NodeJS_MySQL/create_table_demo.js
@@ -1,5 +1,5 @@
 var mysql = require('mysql');
-
+//https://www.w3schools.com/nodejs/nodejs_mysql_create_table.asp
 var con = mysql.createConnection({
     host: "localhost",
     user: "nodejsUser",
@@ -10,9 +10,10 @@ var con = mysql.createConnection({
 con.connect(function (err) {
     if (err) throw err;
     console.log("Connected & Used Database 'mynodejsDB'");
-    
+
     var sql = "CREATE TABLE IF NOT EXISTS " +
-        "customers(name VARCHAR(255), address varchar(255))";
+        "customers(id INT AUTO_INCREMENT PRIMARY KEY, " +
+        "name VARCHAR(255), address varchar(255))";
     con.query(sql, function (err, result) {
         if (err) throw err;
         console.log("Table created!");
