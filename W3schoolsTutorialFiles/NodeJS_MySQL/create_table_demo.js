6a76ac9969edb4fad96b0961bb88cf7ac1ffe679
郵差 <a34007024@gmail.com>
2021-05-09 15:01:56 +0800

    write step-by-step code and close SQL connection

--- W3schoolsTutorialFiles/NodeJS_MySQL/create_table_demo.js
+++ W3schoolsTutorialFiles/NodeJS_MySQL/create_table_demo.js
@@ -1,5 +1,6 @@
-var mysql = require('mysql');
 //https://www.w3schools.com/nodejs/nodejs_mysql_create_table.asp
+var mysql = require('mysql');
+
 var con = mysql.createConnection({
     host: "localhost",
     user: "nodejsUser",
@@ -12,12 +13,21 @@ con.connect(function (err) {
     console.log("Connected & Used Database 'mynodejsDB'");
 
     var sql_createTB = "CREATE TABLE IF NOT EXISTS " +
-        "customers(id INT AUTO_INCREMENT PRIMARY KEY, " +
-        "name VARCHAR(255), address varchar(255))";
+        "customers(name VARCHAR(255), address varchar(255))";
+    con.query(sql_createTB, function (err, result) {
+        if (err) throw err;
+        console.log("Table Created!(Without Primary Key)");
+    });
+
     var sql_alterTB = "ALTER TABLE customers " +
         "ADD COLUMN id INT AUTO_INCREMENT PRIMARY KEY";
     con.query(sql_alterTB, function (err, result) {
         if (err) throw err;
-        console.log("Table altered!");
+        console.log("Table altered!(Add PK Column)");
+    });
+
+    con.end(function (err, result) {
+        if (err) throw err;
+        console.log("Closed SQL Connection, Bye~");
     });
 });
