231bd9e5f4d5871ecae401beca40d6267e14e3de
郵差 <a34007024@gmail.com>
2021-05-09 14:28:25 +0800

    update sql command (check if table exists)

--- W3schoolsTutorialFiles/NodeJS_MySQL/create_table_demo.js
+++ W3schoolsTutorialFiles/NodeJS_MySQL/create_table_demo.js
@@ -10,7 +10,9 @@ var con = mysql.createConnection({
 con.connect(function (err) {
     if (err) throw err;
     console.log("Connected & Used Database 'mynodejsDB'");
-    var sql = "CREATE TABLE customers(name VARCHAR(255), address varchar(255))";
+    
+    var sql = "CREATE TABLE IF NOT EXISTS " +
+        "customers(name VARCHAR(255), address varchar(255))";
     con.query(sql, function (err, result) {
         if (err) throw err;
         console.log("Table created!");
