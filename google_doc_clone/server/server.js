551372d4fe5087120174ce29dcdde4153d5000b3
郵差 <a34007024@gmail.com>
2021-05-12 16:42:11 +0800

    server broadcast changes to make all clients receive the changes

--- google_doc_clone/server/server.js
+++ google_doc_clone/server/server.js
@@ -8,6 +8,6 @@ const io = require('socket.io')(3001, {
 io.on("connection", socket => {
     console.log("connected!");
     socket.on("send-changes", delta => {
-        console.log(delta);
+        socket.broadcast.emit("receive-changes", delta);
     });
 });
