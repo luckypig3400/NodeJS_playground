27cd6a51157a1a0cc30c3e163711f844fc1aba61
郵差 <a34007024@gmail.com>
2021-05-12 13:21:03 +0800

    Update server.js

---
+++ google_doc_clone/server/server.js
@@ -0,0 +1,12 @@
+const { Socket } = require('socket.io');
+
+const io = require('socket.io')(3001, {
+    cors: {
+        origin: 'http://localhost:3000',
+        methods: ['GET', 'POST']
+    }
+});
+
+io.on("connection", socket => {
+
+});
