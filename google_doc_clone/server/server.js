2f26b7149d3efffa6dd338d8c7acdf819a001310
郵差 <a34007024@gmail.com>
2021-05-14 21:05:13 +0800

    load specified document and provide save doc func

--- google_doc_clone/server/server.js
+++ google_doc_clone/server/server.js
@@ -19,12 +19,16 @@ io.on("connection", socket => {
     console.log("connected!");
 
     socket.on('get-document', documentId => {
-        const data = "";
+        const document = await findOrCreateDocument(documentId);
         socket.join(documentId);
-        socket.emit("load-document", data);
+        socket.emit("load-document", document.data);
 
         socket.on("send-changes", delta => {
             socket.broadcast.to(documentId).emit("receive-changes", delta);
+        });
+
+        socket.on("save-document", async data => {
+            await Document.findByIdAndUpdate(documentId, { data });
         });
     });
 
