92477bcd58b8271d91ccd0d2ea634502f690b974
郵差 <twHenryYang@gmail.com>
2021-05-23 22:07:16 +0800

    update readfile demo to show RWD Web

--- W3schoolsTutorialFiles/NodeJS_File_System_Module/demo_readfile.js
+++ W3schoolsTutorialFiles/NodeJS_File_System_Module/demo_readfile.js
@@ -2,7 +2,7 @@ var http = require('http');
 var fs = require('fs');
 
 http.createServer(function (req, res) {
-    fs.readFile('demofile1.html', function (err, data) {
+    fs.readFile('rwd_html_template/index.html', function (err, data) {
         res.writeHead(200, { 'Content-Type': 'text/html' });
         res.write(data);
         return res.end();
