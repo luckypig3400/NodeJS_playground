7d26c0228471dee23c243b2ced3be5efc5270a80
郵差 <twHenryYang@gmail.com>
2021-05-26 12:18:21 +0800

    set static folder for template HTML

--- Express_first_try/app.js
+++ Express_first_try/app.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+
+app.use('/rwd_html_template', express.static('rwd_html_template'))
+
 //ref1: http://expressjs.com/en/starter/hello-world.html
+//ref2: https://developer.mozilla.org/zh-TW/docs/Learn/Server-side/Express_Nodejs/development_environment
 app.get('/', function (req, res) {
     res.send('Hello World');
     // (req, res) => { } is equal to function(req, res) { }
 });
-//ref2: https://developer.mozilla.org/zh-TW/docs/Learn/Server-side/Express_Nodejs/development_environment
+
 app.listen(port, () => {
     console.log('Example app listening at http://localhost:' + port);
 });
