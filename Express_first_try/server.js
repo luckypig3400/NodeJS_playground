49d5de21ce85ee6d3d9c58d1daa3c17b9cb16554
郵差 <twHenryYang@gmail.com>
2021-06-11 19:09:31 +0800

    handle form GET method

--- Express_first_try/server.js
+++ Express_first_try/server.js
@@ -2,11 +2,24 @@ var express = require("express");
 var app = express();
 var bodyParser = require("body-parser");
 
+// 創建application/x-www-form-urlencoded 編碼解析
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
+
 app.use('public', express.static('public'));
 
 app.get('/', function (req, res) {
     res.sendFile(__dirname + "/" + "index.html");
 })
+
+app.get('/process_get', urlencodedParser, function (req, res) {
+    //輸出JSON格式
+    var response = {
+        "first_name": req.query.first_name,
+        "last_name": req.query.last_name
+    };
+    console.log(response);
+    res.end(JSON.stringify(response));
+});
 
 var server = app.listen(8081, function () {
 
@@ -14,5 +27,5 @@ var server = app.listen(8081, function () {
     var port = server.address().port;
 
     console.log("表單接收範例，訪問地址http://%s:%s", host, port);
-    
+
 });
