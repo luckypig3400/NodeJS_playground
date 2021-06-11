6654f2063aed5d7518d4d36d12652291acb54ba7
郵差 <twHenryYang@gmail.com>
2021-06-11 21:24:41 +0800

    handle POST (file upload form)

--- Express_first_try/server.js
+++ Express_first_try/server.js
@@ -3,11 +3,37 @@
 var express = require("express");
 var app = express();
 var bodyParser = require("body-parser");
+var fs = require("fs");
+var multer = require("multer");
 
 // 創建application/x-www-form-urlencoded 編碼解析
 var urlencodedParser = bodyParser.urlencoded({ extended: false });
 
 app.use('public', express.static('public'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(multer({ dest: '/tmp/' }).array('image'));
+
+app.post('/file_upload', function (req, res) {
+
+    console.log(req.files[0]);
+
+    var des_file = __dirname + "/" + req.files[0].originalname;
+
+    fs.readFile(req.files[0].path, function (err, data) {
+        fs.writeFile(des_file, data, function (err) {
+            if (err) {
+                console.log(err);
+            } else {
+                response = {
+                    message: 'File uploaded successfully',
+                    filename: req.files[0].originalname
+                };
+            }
+            console.log(response);
+            res.end(JSON.stringify(response));
+        });
+    });
+});
 
 app.get('/', function (req, res) {
     res.sendFile(__dirname + "/" + "index.html");
