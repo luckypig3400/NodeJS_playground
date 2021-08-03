e1d84fdaadb4c1d9e6e51612a5ae8b50da17fd25
郵差 <twHenryYang@gmail.com>
2021-08-03 15:55:43 +0800

    Create app.js and rename users.js

---
+++ LoginSystem_with_PassportJS/app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const router = express.Router();
+const app = express();
+const mongoose = require('mongoose');
+const expresEjsLayout = require('express-ejs-layouts');
+
+//mongoose
+mongoose.connect('mongodb://localhost/test', { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('Connected,'))
+    .catch((err) => console.log(err));
+
+//EJS
+app.set('view engine', 'ejs');
+app.use(expresEjsLayout);
+//BodyParser
+app.use(express.urlencoded({ extended: false }));
+
+//Routes
+app.use('/', require('./routes/index'));
+app.use('/users', require('./routes/users'));
+
+app.listen(3000, function(){
+    console.log('Server start at http://localhost:3000');
+});
