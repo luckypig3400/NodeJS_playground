08ab9b5f5c19ab8fe59bb5d48dbc14f9f3c92aa8
郵差 <twHenryYang@gmail.com>
2021-08-04 08:39:08 +0800

    Create User Schema and handle register POST

--- LoginSystem_with_PassportJS/routes/users.js
+++ LoginSystem_with_PassportJS/routes/users.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+const User = require("../models/user");//import User model
 
 //login handle
 router.get('/login', (req, res) => {
@@ -11,7 +13,47 @@ router.get('/register', (req, res) => {
 
 //Register handle
 router.post('/register', (req, res) => {
-
+    const { name, email, password, password2 } = req.body;
+    let errors = [];
+    console.log('Name:' + name + ',Email:' + email + 'PWD:' + password);
+    //check if empty
+    if (!name || !email || !password || !password2) {
+        errors.push({ msg: "Please fill in all fields" });
+    }
+    //check if PWD match
+    if (password != password2) {
+        errors.push({ msg: "Password doesn't match" });
+    }
+    //check if PWD is more than 6 characters
+    if (password.length < 6) {
+        errors.push({ msg: 'Password at least 6 characters' })
+    }
+    //If got errors show them to client
+    if (errors.length > 0) {
+        res.render('register', {
+            errors: errors,
+            name: name,
+            email: email,
+            password: password,
+            password2: password2
+        });
+    }
+    //Validation Passed
+    else {
+        User.findOne({ email: email }).exec((err, user) => {
+            console.log(user);
+            if (user) {
+                errors.push({ msg: 'Email already registered!' });
+                render(res, errors, name, email, password, password2);
+            } else {
+                const newUser = new User({
+                    name: name,
+                    email: email,
+                    password: password
+                });
+            }
+        });
+    }
 })
 router.post('/login', (req, res, next) => {
 
