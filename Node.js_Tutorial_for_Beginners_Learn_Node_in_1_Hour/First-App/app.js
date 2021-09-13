53b176260deb6d28ec8bd164ec447545ee336d60
郵差 <a34007024@gmail.com>
2021-09-13 18:09:16 +0800

    Learn about path module

--- Node.js_Tutorial_for_Beginners_Learn_Node_in_1_Hour/First-App/app.js
+++ Node.js_Tutorial_for_Beginners_Learn_Node_in_1_Hour/First-App/app.js
@@ -1,5 +1,12 @@
 const logger = require('./logger'); // use const to avoid reassign var by mistake
+const path = require('path');
 
+console.log('See mudule Obj:');
 console.log(logger); // see property of a module
 
+console.log('Testing our module:');
 logger.log('Hello my tomodachi~');
+
+console.log('Using path module to parse filename:');
+var pathObj = path.parse(__filename);
+console.log(pathObj);
--- Node.js_Tutorial_for_Beginners_Learn_Node_in_1_Hour/First-App/logger.js
+++ Node.js_Tutorial_for_Beginners_Learn_Node_in_1_Hour/First-App/logger.js
@@ -1,6 +1,9 @@
 var url = 'http://google.com';
 
 function log(message) {
+    console.log(__filename);
+    console.log(__dirname);
+
     // send a HTTP request
     console.log(message);
 }
