f9721d3f0461ef9221a3e3fced61751fbfccc153
郵差 <twHenryYang@gmail.com>
2021-09-14 12:00:33 +0800

    practice OS freemem func

--- Node.js_Tutorial_for_Beginners_Learn_Node_in_1_Hour/First-App/app.js
+++ Node.js_Tutorial_for_Beginners_Learn_Node_in_1_Hour/First-App/app.js
@@ -1,5 +1,6 @@
 const logger = require('./logger'); // use const to avoid reassign var by mistake
 const path = require('path');
+const os = require('os');
 
 console.log('See mudule Obj:');
 console.log(logger); // see property of a module
@@ -10,3 +11,6 @@ logger.log('Hello my tomodachi~');
 console.log('Using path module to parse filename:');
 var pathObj = path.parse(__filename);
 console.log(pathObj);
+
+console.log('Using OS module to see OS info:');
+console.log('Free RAM size:' + os.freemem() / 1024 / 1024 + 'MB');
