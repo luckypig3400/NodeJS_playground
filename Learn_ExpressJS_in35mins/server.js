2464274d2b96ddb77aa94ec72acdd699b93f092f
郵差 <twHenryYang@gmail.com>
2021-11-20 22:11:45 +0800

    try Download file and send status code

--- Learn_ExpressJS_in35mins/server.js
+++ Learn_ExpressJS_in35mins/server.js
@@ -3,7 +3,13 @@ const app = express();
 
 app.get('/', (req, res) => {
     console.log("Someone visit /");
-    res.send("<h1>Hi~</h1>");
+    res.send("<h1>Hi~</h1> Everything is fine~ <a href='./download'>Download server file</a>");
+    res.status(200);//500代表internal server error
+});
+
+app.get('/download', (req, res) => {
+    res.download("server.js");
+    console.log("Someone download the server file");
 });
 
 app.listen(3000);
