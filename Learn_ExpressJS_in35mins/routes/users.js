d15cd4576fd5d4279cf903c678339b8883116d3d
郵差 <twHenryYang@gmail.com>
2021-11-21 22:04:22 +0800

    Add Put & Delete route for /users/:id

--- Learn_ExpressJS_in35mins/routes/users.js
+++ Learn_ExpressJS_in35mins/routes/users.js
@@ -20,4 +20,12 @@ router.get("/:id", (req, res) => {
     res.send(`Get User with ID:${req.params.id}`);
 });
 
+router.put("/:id", (req, res) => {
+    res.send(`Update User With ID:${req.params.id}`);
+});
+
+router.delete("/:id", (req, res) => {
+    res.send(`Delete User With ID:${req.params.id}`);
+});
+
 module.exports = router;
