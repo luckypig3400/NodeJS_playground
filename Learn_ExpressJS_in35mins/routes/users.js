e4d69b7f9da9aea8f8b10c693caedea8ab7ccc4c
郵差 <twHenryYang@gmail.com>
2021-11-21 22:00:51 +0800

    Fix dynamic route occupy the specific routes

--- Learn_ExpressJS_in35mins/routes/users.js
+++ Learn_ExpressJS_in35mins/routes/users.js
@@ -5,16 +5,19 @@ router.get("/", (req, res) => {
     res.send("<h1>Users List</h1>");
 });
 
+router.get("/new", (req, res) => {
+    res.send("<h1>User Register Form</h1>");
+});
+// To avoid dynamic route block your specific routes,
+// you should put your dynamic route code below specific one
+// Because the code is run from top to down ~~~
+
 router.post("/", (req, res) => {
     res.send("Create User");
 });
 
 router.get("/:id", (req, res) => {
     res.send(`Get User with ID:${req.params.id}`);
-});
-
-router.get("/new", (req, res) => {
-    res.send("<h1>User Register Form</h1>");
 });
 
 module.exports = router;
