fbedf21c3f30d889ff254d180200f912c02fde1a
郵差 <twHenryYang@gmail.com>
2021-11-21 21:57:20 +0800

    Get dynamic parameters in url (EX:id)

--- Learn_ExpressJS_in35mins/routes/users.js
+++ Learn_ExpressJS_in35mins/routes/users.js
@@ -5,6 +5,14 @@ router.get("/", (req, res) => {
     res.send("<h1>Users List</h1>");
 });
 
+router.post("/", (req, res) => {
+    res.send("Create User");
+});
+
+router.get("/:id", (req, res) => {
+    res.send(`Get User with ID:${req.params.id}`);
+});
+
 router.get("/new", (req, res) => {
     res.send("<h1>User Register Form</h1>");
 });
